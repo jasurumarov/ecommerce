0372adef778053395bc78c216674838cc7804ae1
jasurumarov <jasurumarov199@gmail.com>
2024-03-21 03:20:17 +0500

    finsihed

---
+++ js/server.js
@@ -0,0 +1,254 @@
+export const PRODUCTS = [
+    {
+        id: 1,
+        title: "iPhone 9",
+        description: "An apple mobile which is nothing like apple",
+        oldPrice: 929,
+        price: 549,
+        isNaqd: false,
+        discountPercentage: 12.96,
+        rating: 4.69,
+        stock: 94,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://www.att.com/scmsassets/global/devices/phones/apple/apple-iphone-15-pro-max/carousel/natural-titanium-1.png",
+        images: [
+            "https://cdn.dummyjson.com/product-images/1/1.jpg",
+            "https://cdn.dummyjson.com/product-images/1/2.jpg",
+            "https://cdn.dummyjson.com/product-images/1/3.jpg",
+            "https://cdn.dummyjson.com/product-images/1/4.jpg",
+            "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 2,
+        title: "iPhone 11",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolore illum dolores alias eligendi iste assumenda voluptatum, pariatur nemo sint. Repellat repudiandae sit voluptatem exercitationem molestiae suscipit blanditiis eveniet aut.",
+        oldPrice: 1029,
+        price: 889,
+        isNaqd: true,
+        discountPercentage: 12.96,
+        rating: 4.89,
+        stock: 94,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://www.att.com/scmsassets/global/devices/phones/apple/apple-iphone-15-pro-max/carousel/natural-titanium-1.png",
+        images: [
+            "https://cdn.dummyjson.com/product-images/1/1.jpg",
+            "https://cdn.dummyjson.com/product-images/1/2.jpg",
+            "https://cdn.dummyjson.com/product-images/1/3.jpg",
+            "https://cdn.dummyjson.com/product-images/1/4.jpg",
+            "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 3,
+        title: "iPhone 14 Pro",
+        description: "An apple mobile which is nothing like apple",
+        oldPrice: 1249,
+        price: 889,
+        isNaqd: false,
+        discountPercentage: 12.96,
+        rating: 4.29,
+        stock: 94,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://www.att.com/scmsassets/global/devices/phones/apple/apple-iphone-15-pro-max/carousel/natural-titanium-1.png",
+        images: [
+            "https://cdn.dummyjson.com/product-images/1/1.jpg",
+            "https://cdn.dummyjson.com/product-images/1/2.jpg",
+            "https://cdn.dummyjson.com/product-images/1/3.jpg",
+            "https://cdn.dummyjson.com/product-images/1/4.jpg",
+            "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 4,
+        title: "iPhone 8 Plus",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolore illum dolores alias eligendi iste assumenda voluptatum, pariatur nemo sint. Repellat repudiandae sit voluptatem exercitationem molestiae suscipit blanditiis eveniet aut.",
+        oldPrice: 519,
+        price: 379,
+        isNaqd: true,
+        discountPercentage: 12.96,
+        rating: 4.99,
+        stock: 94,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://www.att.com/scmsassets/global/devices/phones/apple/apple-iphone-15-pro-max/carousel/natural-titanium-1.png",
+        images: [
+            "https://cdn.dummyjson.com/product-images/1/1.jpg",
+            "https://cdn.dummyjson.com/product-images/1/2.jpg",
+            "https://cdn.dummyjson.com/product-images/1/3.jpg",
+            "https://cdn.dummyjson.com/product-images/1/4.jpg",
+            "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 5,
+        title: "iPhone 8 Plus",
+        description: "An apple mobile which is nothing like apple",
+        oldPrice: 519,
+        price: 379,
+        isNaqd: false,
+        discountPercentage: 12.96,
+        rating: 4.99,
+        stock: 94,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://www.att.com/scmsassets/global/devices/phones/apple/apple-iphone-15-pro-max/carousel/natural-titanium-1.png",
+        images: [
+            "https://cdn.dummyjson.com/product-images/1/1.jpg",
+            "https://cdn.dummyjson.com/product-images/1/2.jpg",
+            "https://cdn.dummyjson.com/product-images/1/3.jpg",
+            "https://cdn.dummyjson.com/product-images/1/4.jpg",
+            "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 6,
+        title: "iPhone 14 Pro",
+        description: "An apple mobile which is nothing like apple",
+        oldPrice: 1249,
+        price: 889,
+        isNaqd: false,
+        discountPercentage: 12.96,
+        rating: 4.29,
+        stock: 94,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://www.att.com/scmsassets/global/devices/phones/apple/apple-iphone-15-pro-max/carousel/natural-titanium-1.png",
+        images: [
+            "https://cdn.dummyjson.com/product-images/1/1.jpg",
+            "https://cdn.dummyjson.com/product-images/1/2.jpg",
+            "https://cdn.dummyjson.com/product-images/1/3.jpg",
+            "https://cdn.dummyjson.com/product-images/1/4.jpg",
+            "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 7,
+        title: "iPhone 11",
+        description: "An apple mobile which is nothing like apple",
+        oldPrice: 1029,
+        price: 889,
+        isNaqd: true,
+        discountPercentage: 12.96,
+        rating: 4.89,
+        stock: 94,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://www.att.com/scmsassets/global/devices/phones/apple/apple-iphone-15-pro-max/carousel/natural-titanium-1.png",
+        images: [
+            "https://cdn.dummyjson.com/product-images/1/1.jpg",
+            "https://cdn.dummyjson.com/product-images/1/2.jpg",
+            "https://cdn.dummyjson.com/product-images/1/3.jpg",
+            "https://cdn.dummyjson.com/product-images/1/4.jpg",
+            "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 8,
+        title: "iPhone 9",
+        description: "An apple mobile which is nothing like apple",
+        oldPrice: 929,
+        price: 549,
+        isNaqd: false,
+        discountPercentage: 12.96,
+        rating: 4.69,
+        stock: 94,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://www.att.com/scmsassets/global/devices/phones/apple/apple-iphone-15-pro-max/carousel/natural-titanium-1.png",
+        images: [
+            "https://cdn.dummyjson.com/product-images/1/1.jpg",
+            "https://cdn.dummyjson.com/product-images/1/2.jpg",
+            "https://cdn.dummyjson.com/product-images/1/3.jpg",
+            "https://cdn.dummyjson.com/product-images/1/4.jpg",
+            "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 9,
+        title: "iPhone 9",
+        description: "An apple mobile which is nothing like apple",
+        oldPrice: 929,
+        price: 549,
+        isNaqd: false,
+        discountPercentage: 12.96,
+        rating: 4.69,
+        stock: 94,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://www.att.com/scmsassets/global/devices/phones/apple/apple-iphone-15-pro-max/carousel/natural-titanium-1.png",
+        images: [
+            "https://cdn.dummyjson.com/product-images/1/1.jpg",
+            "https://cdn.dummyjson.com/product-images/1/2.jpg",
+            "https://cdn.dummyjson.com/product-images/1/3.jpg",
+            "https://cdn.dummyjson.com/product-images/1/4.jpg",
+            "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 10,
+        title: "iPhone 11",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolore illum dolores alias eligendi iste assumenda voluptatum, pariatur nemo sint. Repellat repudiandae sit voluptatem exercitationem molestiae suscipit blanditiis eveniet aut.",
+        oldPrice: 1029,
+        price: 889,
+        isNaqd: true,
+        discountPercentage: 12.96,
+        rating: 4.89,
+        stock: 94,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://www.att.com/scmsassets/global/devices/phones/apple/apple-iphone-15-pro-max/carousel/natural-titanium-1.png",
+        images: [
+            "https://cdn.dummyjson.com/product-images/1/1.jpg",
+            "https://cdn.dummyjson.com/product-images/1/2.jpg",
+            "https://cdn.dummyjson.com/product-images/1/3.jpg",
+            "https://cdn.dummyjson.com/product-images/1/4.jpg",
+            "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 11,
+        title: "iPhone 14 Pro",
+        description: "An apple mobile which is nothing like apple",
+        oldPrice: 1249,
+        price: 889,
+        isNaqd: true,
+        discountPercentage: 12.96,
+        rating: 4.29,
+        stock: 94,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://www.att.com/scmsassets/global/devices/phones/apple/apple-iphone-15-pro-max/carousel/natural-titanium-1.png",
+        images: [
+            "https://cdn.dummyjson.com/product-images/1/1.jpg",
+            "https://cdn.dummyjson.com/product-images/1/2.jpg",
+            "https://cdn.dummyjson.com/product-images/1/3.jpg",
+            "https://cdn.dummyjson.com/product-images/1/4.jpg",
+            "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 12,
+        title: "iPhone 8 Plus",
+        description: "An apple mobile which is nothing like apple",
+        oldPrice: 519,
+        price: 379,
+        isNaqd: true,
+        discountPercentage: 12.96,
+        rating: 4.99,
+        stock: 94,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://www.att.com/scmsassets/global/devices/phones/apple/apple-iphone-15-pro-max/carousel/natural-titanium-1.png",
+        images: [
+            "https://cdn.dummyjson.com/product-images/1/1.jpg",
+            "https://cdn.dummyjson.com/product-images/1/2.jpg",
+            "https://cdn.dummyjson.com/product-images/1/3.jpg",
+            "https://cdn.dummyjson.com/product-images/1/4.jpg",
+            "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
+        ]
+    },
+]
